df5d5e864d5bfc2f66a44335141ae049e94774a6
Jiaxing Li <henrylovewatermelon@gmail.com>
2022-02-09 22:19:25 -0500

    first commit

---
+++ DataExploration.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb  9 21:17:53 2022
+
+@author: Jiaxing Li
+"""
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+data = pd.read_json('./Office_Products_5.json', lines=True)
+
+data.head(3)
+data.describe()
+data.shape
+data.dtypes
+data.columns
+data.columns.values
+data.info()
+data.keys()
+data.isnull().sum()
+data.columns
+
+data['overall'].describe()
+
+# Distribution of the number of reviews per product
+productReviewCount = data.groupby(['asin']).size()
+productReviewCount.plot.hist(bins=12, alpha=0.5)
+
+# Distribution of reviews per user
+userReviewCount = data.groupby(['reviewerID']).size()
+userReviewCount.plot.hist(bins=12, alpha=0.5)
+
+# Distribution of all reviews
+review = pd.DataFrame(data.groupby(['overall']).size())
+
+# Distribution of review text length
+reviewLength = data['reviewText'].apply(len)
+reviewLength.plot.hist(bins=2000, alpha=0.5)
