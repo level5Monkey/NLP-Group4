a0c3d0084ad187776d06ccb2ea1f946498eeb400
Jiaxing Li <henrylovewatermelon@gmail.com>
2022-02-09 22:45:51 -0500

    add xlabel

--- DataExploration.py
+++ DataExploration.py
@@ -26,14 +26,20 @@ data['overall'].describe()
 # Distribution of the number of reviews per product
 productReviewCount = data.groupby(['asin']).size()
 productReviewCount.plot.hist(bins=12, alpha=0.5)
+plt.xlabel('review count per product')
 
 # Distribution of reviews per user
 userReviewCount = data.groupby(['reviewerID']).size()
 userReviewCount.plot.hist(bins=12, alpha=0.5)
+plt.xlabel('review count per user')
 
 # Distribution of all reviews
 review = pd.DataFrame(data.groupby(['overall']).size())
+review['rating'] = [1,2,3,4,5]
+review.plot(x='rating',kind='bar')
+plt.xlabel('rating')
 
 # Distribution of review text length
 reviewLength = data['reviewText'].apply(len)
 reviewLength.plot.hist(bins=2000, alpha=0.5)
+plt.xlabel('length per review')
